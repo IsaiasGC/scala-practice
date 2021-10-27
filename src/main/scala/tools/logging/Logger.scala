f26ec118b4b8224820aec9c40d65d3aa5f8a990f
Isaias Gonzalez <igonzalez@gobdigital.com>
2021-10-27 13:07:39 -0500

    [UPDATE[Logger] Cambio de nombres de metodos del logger

--- src/main/scala/tools/logging/Logger.scala
+++ src/main/scala/tools/logging/Logger.scala
@@ -8,7 +8,7 @@ object Logger {
   private def fecha = inputFormat.format(new Date())
 
   def info(message: Any): Unit = System.out.println(s"$fecha [INFO] $message")
-  def error(message: Any): Unit = System.err.println(s"$fecha [ERRO] $message")
-  def warning(message: Any): Unit =
+  def erro(message: Any): Unit = System.err.println(s"$fecha [ERRO] $message")
+  def warn(message: Any): Unit =
     System.out.println(s"$fecha [WARN] $message")
 }
